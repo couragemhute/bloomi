793e10576ee8fb39cdf1b5425f312a5f9271cc14
mist_coat <couragemhute21@gmail.com>
2025-01-15 10:22:33 +0200

    add procfile

--- ultimate_creative/settings.py
+++ ultimate_creative/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_$^5usrl&@1+cg-avl$a%0bu5kt8hoo(+)*2ix^mr4enstkt_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1", "ultimate-creative-website.onrender.com", '0.0.0.0']
 
