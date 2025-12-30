434cb94d943691eac78bc8e2f3c4b0aafcfec387
mist_coat <couragemhute21@gmail.com>
2025-12-30 08:48:27 +0200

    fix email and add yvonne

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,8 +2,9 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from accounts.models import CustomUser
+from role.mixins.role_permission_required import RolePermissionRequiredMixin
 
-class DashboardListView(LoginRequiredMixin,TemplateView):
+class DashboardListView(LoginRequiredMixin,RolePermissionRequiredMixin,TemplateView):
     template_name = 'dashboard/dashboard.html'
 
     def get_context_data(self, **kwargs):
@@ -13,4 +14,7 @@ class DashboardListView(LoginRequiredMixin,TemplateView):
         users = CustomUser.objects.all()
  
         return context
+    
+    def get_required_permissions(self):
+        return ["view_customuser"]
 
