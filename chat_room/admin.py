5cd465c9d2b8bb4d72a879cdaa637c3778134a11
mist_coat <couragemhute21@gmail.com>
2026-01-09 09:01:07 +0200

    add chatroom modls to admin

--- chat_room/admin.py
+++ chat_room/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ChatRoom, ChatParticipant, ChatMessage
 
-# Register your models here.
+
+admin.site.register(ChatRoom)
+admin.site.register(ChatParticipant)
+admin.site.register(ChatMessage)
--- enrollment/admin.py
+++ enrollment/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CourseEnrollment
 
-# Register your models here.
+
+admin.site.register(CourseEnrollment)
